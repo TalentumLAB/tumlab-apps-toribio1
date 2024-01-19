4c47080bcb7429c6ccaee6f7f26bb21aed6d98ea
Jose Ramiro Ramirez Vargas <117289972+espserver@users.noreply.github.com>
2024-01-19 07:01:10 -0500

    Update const.js

--- const.js
+++ const.js
@@ -161,7 +161,7 @@ export const headerMenulist = [
   {
     id: "talentum-resources",
     name: `${header["header-category-talentum-resources"]}`,
-    is_enable: false,
+    is_enable: true,
   },
   {
     id: "digital-tools",
@@ -212,7 +212,7 @@ export const apps = [
       {
         id: "steam-classrooms-patia",
         order: 3,
-        is_show: true,
+        is_show: false,
         thumbnail: "./assets/recursos-talentum/patia_thumbnail.webp",
         video: "./assets/recursos-talentum/videos/recursos-talentum-patia.mp4",
         title: `${sectionApps["app-steam-classrooms-patia-title"]}`,
@@ -222,7 +222,7 @@ export const apps = [
       {
         id: "steam-classrooms-valle",
         order: 4,
-        is_show: true,
+        is_show: false,
         thumbnail: "./assets/recursos-talentum/steam-valle_thumbnail.webp",
         video:
           "./assets/recursos-talentum/videos/recursos-talentum-steam-valle.mp4",
@@ -239,7 +239,7 @@ export const apps = [
       {
         id: "universum",
         order: 1,
-        is_show: true,
+        is_show: false,
         thumbnail: "./assets/herramientas-digitales/universum_thumbnail.webp",
         video:
           "./assets/herramientas-digitales/videos/herramientas-digitales-universum.mp4",
@@ -266,12 +266,12 @@ export const apps = [
       {
         id: "simulators-spanish",
         order: 2,
-        is_show: false,
+        is_show: true,
         thumbnail: "./assets/simuladores/simulators-spanish_thumbnail.webp",
         video: "./assets/simuladores/videos/simulators-spanish.mov",
         title: `${sectionApps["app-simulators-spanish-title"]}`,
         description: `${sectionApps["app-simulators-spanish-description"]}`,
-        url: "http://tumlab.local:8087/phet_es_2022-12/A/index.html",
+        url: "http://tumlab.local:8087/viewer#phet_es_2023-01/A/index.html",
       },
       {
         id: "simulators-english",
@@ -291,7 +291,7 @@ export const apps = [
       {
         id: "apache",
         order: 1,
-        is_show: true,
+        is_show: false,
         thumbnail: "./assets/infraestructura-datos/superset_thumbnail.webp",
         video:
           "./assets/infraestructura-datos/videos/infraestructura-datos-apache-superset.mp4",
@@ -314,7 +314,7 @@ export const apps = [
           "./assets/recursos-educativos/videos/recursos-educativos-wikipedia.mp4",
         title: `${sectionApps["app-wikipedia-title"]}`,
         description: `${sectionApps["app-wikipedia-description"]}`,
-        url: "http://tumlab.local:8087/viewer#wikipedia_en_all_maxi_2023-11/A/User:The_other_Kiwix_guy/Landing",
+        url: "http://tumlab.local:8087/viewer#wikipedia_es_all_maxi_2023-10/A/Wikipedia:Offline",
       },
 
       {
@@ -359,7 +359,7 @@ export const apps = [
           "./assets/recursos-educativos/videos/recursos-educativos-krita.mp4",
         title: `${sectionApps["app-krita-title"]}`,
         description: `${sectionApps["app-krita-description"]}`,
-        url: "http://192.168.4.1:8086",
+        url: "http://tumlab.local/Krita/krita-x64-5.2.2-setup.exe",
       },
     ],
   },
